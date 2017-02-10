6ff1af0a8c0ec1d0c200a3f2708333dcab06e91f
Paige Pfeiffer <ppfeiffe@macalester.edu>
2017-02-10 15:08:43 -0600

    Possible StepSquare class

---
+++ Stepquencer/Stepquencer/StepSquares.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Stepquencer
+{
+	public class StepSquares
+	{
+		private double SQUARE_WIDTH = 20.0;
+		private double SQUARE_LENGTH = 20.0;
+		private double x;
+		private double y;
+		private Color c;
+
+		public StepSquares(double x, double y, Color c)
+		{
+			this.x = x;
+			this.y = y;
+			this.c = c;
+		}
+
+		public double getSquareWidth() { return SQUARE_WIDTH; }
+		public double getSquareLength() { return SQUARE_LENGTH; }
+	}
+}
