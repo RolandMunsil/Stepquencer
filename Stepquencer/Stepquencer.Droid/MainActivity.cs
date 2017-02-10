4245a3dda702b868f323707a22206d4c9b216726
Roland Munsil <roland@munsil.com>
2017-02-10 14:33:35 -0600

    Got sounds working on Android!
    It will play a "song" on repeat when the app starts up. You can disable
    it by commenting out the while loop in MainActivity.cs
    Also:
    * Project build target is now Android 7.0
    * Disabled fast deployment of the Android build as it was causing issues
    for me.

--- Stepquencer/Stepquencer.Droid/MainActivity.cs
+++ Stepquencer/Stepquencer.Droid/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Media;
 
 namespace Stepquencer.Droid
 {
@@ -18,7 +19,73 @@ namespace Stepquencer.Droid
 
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (new Stepquencer.App ());
-		}
-	}
+
+            AudioTrack timTrack = AudioTrackFromRawResource(Resource.Raw.tim);
+            AudioTrack echTrack = AudioTrackFromRawResource(Resource.Raw.ech);
+
+            int i = 0;
+            while (true)
+            {
+                System.Threading.Thread.Sleep(500);
+
+                //TODO: Does it matter whether you use Stop() vs. Pause()?
+                timTrack.Pause();
+
+                //Size of WAV header is 44 bytes
+                timTrack.SetPlaybackHeadPosition(44);
+                timTrack.SetPlaybackRate((int)(44100 * Math.Pow(2, (i%12) / 12.0)));
+
+                if(i%2 == 1)
+                {
+                    //Resetting takes about 0.3-2.0ms (in debug mode)
+                    //Playing takes about 0.5-1ms (in debug mode)
+
+                    echTrack.Pause();
+                    echTrack.SetPlaybackHeadPosition(44);
+                    echTrack.Play();
+                }
+
+                timTrack.Play();
+
+                i++;
+            }
+            //audioTrack.Dispose();
+        }
+
+        private AudioTrack AudioTrackFromRawResource(int resourceID)
+        {
+            int fileLength;
+            using (Android.Content.Res.AssetFileDescriptor fileInfo = Resources.OpenRawResourceFd(resourceID))
+            {
+                fileLength = (int)fileInfo.Length;
+            }
+
+            byte[] audioBuffer;
+            using (System.IO.Stream stream = Resources.OpenRawResource(resourceID))
+            {
+                using (System.IO.BinaryReader br = new System.IO.BinaryReader(stream))
+                {
+                    audioBuffer = br.ReadBytes(fileLength);
+                }
+            }
+
+            AudioTrack audioTrack = new AudioTrack(
+                // Stream type
+                Stream.Music,
+                // Frequency
+                44100,
+                // Mono or stereo
+                ChannelOut.Stereo,
+                // Audio encoding
+                Encoding.Pcm16bit,
+                // Length of the audio clip.
+                audioBuffer.Length,
+                // Mode. Stream or static.
+                AudioTrackMode.Static);
+
+            audioTrack.Write(audioBuffer, 0, audioBuffer.Length, WriteMode.Blocking);
+            return audioTrack;
+        }
+    }
 }
 
