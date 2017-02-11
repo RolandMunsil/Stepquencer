e1fb52116db1c6e2c42c2e15c4fc07efab370a00
Gabe <thosemovieguys2015@gmail.com>
2017-02-11 15:20:39 -0600

    Test buttons now appear on main page

--- Stepquencer/Stepquencer/MainPage.xaml.cs
+++ Stepquencer/Stepquencer/MainPage.xaml.cs
@@ -16,27 +16,48 @@ namespace Stepquencer
 		{
 			InitializeComponent();
 
+
+			BackgroundColor = Color.FromHex("#404040");
+
+			Style plainButton = new Style(typeof(Button))
+			{
+				Setters = 
+				{
+	  				new Setter { Property = Button.BackgroundColorProperty, Value = Color.FromHex ("#eee") },
+	  				new Setter { Property = Button.TextColorProperty, Value = Color.Black },
+	  				new Setter { Property = Button.BorderRadiusProperty, Value = 0 },
+	  				new Setter { Property = Button.FontSizeProperty, Value = 40 }
+				}
+			};
+
 			//Set up grid of StepSquares
 
-			var stepgrid = new Grid();
+			Grid stepgrid = new Grid();
 
 
 			//Initialize the number of rows and columns
 			for (int i = 0; i < NumRows; i++)
 			{
-				stepgrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+				stepgrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
 			}
 
 			for (int i = 0; i < NumColumns; i++)
 			{
-				stepgrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) });
+				stepgrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
 			}
 
 			//Add the buttons to it
-			for (int i = 0; i < NumRows * NumColumns; i++)
+			for (int i = 0; i < NumRows; i++)
 			{
-				stepgrid.Children.Add(new Button { Text = "WOOP" });
+				for (int j = 0; j < NumColumns; j++)
+				{
+					stepgrid.Children.Add(new Button { Style = plainButton}, i, j);
+				}
 			}
+
+			//var label = new Label { Text = "This should show up with music", TextColor = Color.FromHex("#77fd65"), FontSize = 20 };
+
+			Content = stepgrid;
 		}
 	}
 }
