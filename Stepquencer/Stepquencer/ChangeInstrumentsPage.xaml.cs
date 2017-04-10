042be1a37d43bdaca29d7833015c5ea87c8313af
Gabe <thosemovieguys2015@gmail.com>
2017-04-10 14:04:32 -0500

    Set up blank changeInstruments page and added a button to get there
    Also made an empty event listener and hooked it up to the undo clear
    button

---
+++ Stepquencer/Stepquencer/ChangeInstrumentsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Stepquencer
+{
+    public partial class ChangeInstrumentsPage : ContentPage
+    {
+        public ChangeInstrumentsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
