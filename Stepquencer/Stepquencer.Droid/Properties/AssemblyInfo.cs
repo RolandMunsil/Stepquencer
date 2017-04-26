8c8e9d359db27fd4f28c56f33a8c27e8ba41b38d
Mani-Diaz <ediaz1@macalester.edu>
2017-04-25 23:02:28 -0500

    Revert "Changed name of icon to newicon4.png and deleted extra class"
    This reverts commit 51dc53b3315960c632854b476b6cd0f7b8982ab3.

--- Stepquencer/Stepquencer.Droid/Properties/AssemblyInfo.cs
+++ Stepquencer/Stepquencer.Droid/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using Android.App;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Application(Icon = "@drawable/NewIcon4")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
