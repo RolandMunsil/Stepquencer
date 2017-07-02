c19b54406bb646c20c2f26bc88e0bbb0a6fdac9f
RolandMunsil <roland@munsil.com>
2017-07-02 15:01:06 -0700

    Rewrote credits page stuff in new format

--- Stepquencer/Stepquencer/Pages/InfoPage.xaml.cs
+++ Stepquencer/Stepquencer/Pages/InfoPage.xaml.cs
@@ -9,99 +9,21 @@ namespace Stepquencer
 {
     public partial class InfoPage : ContentPage
     {
-
-        private static List<Author> authors;
-
         public InfoPage()
         {
             this.BackgroundColor = Color.Black;
             StackLayout pageLayout = new StackLayout { Orientation = StackOrientation.Vertical };
 
-            ReadAuthorData();
-            foreach (LinkLabel label in ConstructLabels())
+            using (StreamReader stream = new StreamReader(FileUtilities.LoadEmbeddedResource("credits.txt")))
             {
-                pageLayout.Children.Add(label);
+                while (stream.Peek() != -1)
+                {
+                    String[] parts = stream.ReadLine().Split('|');
+                    pageLayout.Children.Add(new LinkLabel(parts[0], parts[1]));
+                }
             }
-
-            pageLayout.Children.Add(new LinkLabel()
-            {
-                Text = "All sounds except clap are from the soundfont \"SGM - V2.01\" by David Shan",
-                LinkText = "http://www.geocities.jp/shansoundfont/"
-            });
-
-            pageLayout.Children.Add(new LinkLabel()
-            {
-                Text = "Clap sound is \"Clap-Clean Reverb\" by Freesound user young_daddy",
-                LinkText = "http://freesound.org/people/young_daddy/"
-            });
 
             this.Content = pageLayout;
-        }
-
-        /// <summary>
-        /// Returns a list a linklabels constructed from the Author objects in authors
-        /// </summary>
-        /// <returns>The labels.</returns>
-        private List<LinkLabel> ConstructLabels()
-        {
-            List<LinkLabel> linkLabels = new List<LinkLabel>();
-            //Author prevAuthor = new Author();
-
-
-            // Add an affiliate link for the first author
-            Author firstAuthor = authors.ElementAt(0);
-
-            LinkLabel firstAffiliate = new LinkLabel();
-            //firstAffiliate.Text = firstAuthor.Name.Equals(firstAuthor.AffiliateName) ? firstAuthor.Name : firstAuthor.AffiliateName;
-            //firstAffiliate.TextColor = Color.White;
-            // Other style settings
-            //linkLabels.Add(firstAffiliate);
-
-            for (int i = 0; i < authors.Count; i++)
-            {
-                Author currentAuthor = authors.ElementAt(i);
-                /*
-                if (i > 0 && !prevAuthor.AffiliateLink.Equals(currentAuthor.AffiliateLink))     // If current author is affiliated with different site/group
-                {
-                    LinkLabel affiliateLabel = new LinkLabel();
-                    affiliateLabel.Text = currentAuthor.AffiliateName;
-                    affiliateLabel.TextColor = Color.White;
-                    //Other style choices
-                    linkLabels.Add(affiliateLabel);
-                }
-                */
-
-
-                linkLabels.Add(new LinkLabel(currentAuthor));
-                //prevAuthor = currentAuthor;
-            }
-
-            return linkLabels;
-        }
-
-        /// <summary>
-        /// Reads in data about authors, instantiates Author objects in list of authors
-        /// </summary>
-        private void ReadAuthorData()
-        {
-            authors = new List<Author>();
-            
-            //Read in data
-            using (StreamReader stream = new StreamReader(FileUtilities.LoadEmbeddedResource("_iconAuthors.txt")))
-            {
-                while (stream.Peek() != -1) //While there are no more lines left to read
-                {
-                    string[] authorInfo = stream.ReadLine().Split('|');
-                    if (authorInfo.Length < 4)
-                    {
-                        authors.Add(new Author(authorInfo[0], authorInfo[1], authorInfo[2]));
-                    }
-                    else
-                    {
-                        authors.Add(new Author(authorInfo[0], authorInfo[1], authorInfo[2], authorInfo[3]));
-                    }
-                }
-            }
         }
     }
 }
--- Stepquencer/Stepquencer/UIElements/LinkLabel.cs
+++ Stepquencer/Stepquencer/UIElements/LinkLabel.cs
@@ -7,63 +7,20 @@ namespace Stepquencer
     /// </summary>
     public class LinkLabel : Label
     {
-        private string linkText;
         private TapGestureRecognizer gestureRecognizer;
-
-        public string LinkText
-        {
-            get { return linkText;}
-            set { linkText = value;}
-        }
 
         /// <summary>
         /// Constructs a generic LinkLabel, where values must be set afterwards
         /// </summary>
-        public LinkLabel() : base()
+        public LinkLabel(String text, String link) : base()
         {
-            this.linkText = "";
+            this.Text = text;
             this.TextColor = Color.Blue;    // Default to traditional link color
 
             this.gestureRecognizer = new TapGestureRecognizer();
             gestureRecognizer.Tapped += (sender, e) =>
             {
-                try
-                {
-                    Device.OpenUri(new Uri(((LinkLabel)sender).LinkText));
-                }
-                catch (Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine(ex + "\n\nMake sure you set the link text correctly!");
-                }
-            };
-
-            this.GestureRecognizers.Add(gestureRecognizer);
-        }
-
-        /// <summary>
-        /// A linklabel initialized to credit an author and link to their personal site
-        /// </summary>
-        /// <param name="author">Author.</param>
-        public LinkLabel(Author author)
-        {
-            String text = author.HasMultipleIcons ? "Icons made by " : "Icon made by ";
-            text += author.Name;
-
-            this.Text = text;
-            this.linkText = author.PersonalLink;
-            this.TextColor = Color.Blue;
-            this.gestureRecognizer = new TapGestureRecognizer();
-
-			gestureRecognizer.Tapped += (sender, e) =>
-			{
-	            try
-	            {
-		            Device.OpenUri(new Uri(((LinkLabel)sender).LinkText));
-	            }
-	            catch (Exception ex)
-	            {
-		            System.Diagnostics.Debug.WriteLine(ex + "\n\nMake sure you set the link text correctly!");
-	            }
+                Device.OpenUri(new Uri(link));
             };
 
             this.GestureRecognizers.Add(gestureRecognizer);
